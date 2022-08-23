dafaaad47de8cf040715588ad985d8b3eeb39d0c
AndreassenLearn <simo531i@elevcampus.dk>
2022-08-23 17:13:01 +0200

    Added text-to-speech on PropertyDetailPage

---
+++ RealEstateApp/RealEstateApp/Converters/BoolToHeightRequestZeroOnFalse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RealEstateApp.Converters
+{
+  public class BoolToHeightRequestZeroOnFalse : IValueConverter, IMarkupExtension
+  {
+    public object ProvideValue(IServiceProvider serviceProvider)
+    {
+      return this;
+    }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      // Validate and convert input.
+      if (value is bool boolean && boolean)
+      {
+        return -1;
+      }
+
+      return 0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotSupportedException();
+    }
+  }
+}
--- RealEstateApp/RealEstateApp/Views/PropertyDetailPage.xaml.cs
+++ RealEstateApp/RealEstateApp/Views/PropertyDetailPage.xaml.cs
@@ -1,34 +1,65 @@
 ﻿using RealEstateApp.Models;
 using RealEstateApp.Services;
 using System.Linq;
+using System.Threading;
 using TinyIoC;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace RealEstateApp.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PropertyDetailPage : ContentPage
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class PropertyDetailPage : ContentPage
+  {
+    public PropertyDetailPage(PropertyListItem propertyListItem)
     {
-        public PropertyDetailPage(PropertyListItem propertyListItem)
-        {
-            InitializeComponent();
+      InitializeComponent();
 
-            Property = propertyListItem.Property;
+      Property = propertyListItem.Property;
 
-            IRepository Repository = TinyIoCContainer.Current.Resolve<IRepository>();
-            Agent = Repository.GetAgents().FirstOrDefault(x => x.Id == Property.AgentId);
+      IRepository Repository = TinyIoCContainer.Current.Resolve<IRepository>();
+      Agent = Repository.GetAgents().FirstOrDefault(x => x.Id == Property.AgentId);
 
-            BindingContext = this;
-        }
-
-        public Agent Agent { get; set; }
-
-        public Property Property { get; set; }
-
-        private async void EditProperty_Clicked(object sender, System.EventArgs e)
-        {
-            await Navigation.PushAsync(new AddEditPropertyPage(Property));
-        }
+      BindingContext = this;
     }
+
+    public Agent Agent { get; set; }
+    public Property Property { get; set; }
+    public SpeechOptions SpeechOptions { get; set; } = new SpeechOptions() { Volume = .5f, Pitch = 1.0f };
+    public bool IsSpeaking { get; set; } = false;
+    //public bool IsSpeaking
+    //{
+    //  get
+    //  {
+    //    return cts?.IsCancellationRequested ?? true;
+    //  }
+    //}
+
+    private CancellationTokenSource cts;
+
+    private async void EditProperty_Clicked(object sender, System.EventArgs e)
+    {
+      await Navigation.PushAsync(new AddEditPropertyPage(Property));
+    }
+
+    private async void TextToSpeechPlay_Clicked(object sender, System.EventArgs e)
+    {
+      cts = new CancellationTokenSource();
+      IsSpeaking = true;
+      await TextToSpeech.SpeakAsync(Property.Description, SpeechOptions, cancelToken: cts.Token);
+      IsSpeaking = false;
+    }
+
+    private void TextToSpeechStop_Clicked(object sender, System.EventArgs e)
+    {
+      if (cts?.IsCancellationRequested ?? true)
+      {
+        return;
+      }
+
+      IsSpeaking = false;
+      cts.Cancel();
+    }
+  }
 }
